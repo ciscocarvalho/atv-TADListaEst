bbdbefa6665404b17e200e3d28b6bef619f9fd18
Francisco <francisco.carvalho.q@gmail.com>
2022-12-05 23:54:18 -0300

    implementar funcoes restantes

--- lista.c
+++ lista.c
@@ -48,3 +48,42 @@ void lista_insert(Lista *l, int valor) {
 int lista_get(Lista *l, int index) {
     return l->vet[index];
 }
+
+void lista_insert_sorted(Lista *l, int valor) {
+    lista_append(l, valor);
+    int valor_index = l->quant - 1;
+
+    for (int i = l->quant - 1; i >= 0; i--) {
+        if (valor < l->vet[i]) {
+            lista_trocar(l, valor_index, i);
+            valor_index = i;
+        }
+    }
+};
+
+int lista_full(Lista *l) {
+    return l->quant == MAX;
+};
+
+int lista_empty(Lista *l) {
+    return l->quant == 0;
+};
+
+void lista_remove(Lista *l, int index) {
+    if (l->quant - 1 <= index) {
+        for (int i = index; i <= l->quant - 2; i++) {
+            lista_trocar(l, i, i + 1);
+        }
+        l->quant--;
+    }
+};
+
+int lista_size(Lista *l) {
+    return l->quant;
+};
+
+void lista_print(Lista *l){
+    for (int i = 0; i < l->quant - 1; i++) {
+        printf("%d\n", l->vet[i]);
+    }
+}

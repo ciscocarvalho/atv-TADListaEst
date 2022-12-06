48179f5be50a5bf2d06023ffc60a3f88b05e4c8b
Francisco <francisco.carvalho.q@gmail.com>
2022-12-05 23:09:44 -0300

    Prefixar funcoes com lista_ para evitar conflitos de nomes
    Ex.: a funcao remove conflita com uma funcao de mesmo nome em <stdio.h>

--- lista.c
+++ lista.c
@@ -9,7 +9,7 @@ struct lista {
 };
 
 
-Lista *criar() {
+Lista *lista_criar() {
     Lista *l;
     l = (Lista *) malloc(sizeof(Lista));
     if (l != NULL) {
@@ -20,16 +20,16 @@ Lista *criar() {
 
 }
 
-void clear(Lista *l) {
+void lista_clear(Lista *l) {
     free(l);
 }
 
-void append(Lista *l, int valor) {
+void lista_append(Lista *l, int valor) {
     l->vet[l->quant] = valor;
     l->quant++;
 }
 
-void insert(Lista *l, int valor) {
+void lista_insert(Lista *l, int valor) {
     
     for (int i = l->quant - 1; i >= 0; i--) {
         l->vet[i + 1] = l->vet[i];
@@ -39,6 +39,6 @@ void insert(Lista *l, int valor) {
 
 }
 
-int get(Lista *l, int index) {
+int lista_get(Lista *l, int index) {
     return l->vet[index];
 }
--- lista.h
+++ lista.h
@@ -2,15 +2,15 @@
 
 typedef struct lista Lista;
 
-Lista *criar();
+Lista *lista_criar();
 
-void append(Lista *l, int valor);
-void insert(Lista *l, int valor);
-void insert_sorted(Lista *l, int valor); // inserir ordenado
-void clear(Lista *l);
-int get(Lista *l, int index);
-int full(Lista *l); // está cheia?
-int empty(Lista *l); // está vazia?
-void remove(Lista *l, int index); // remover por índice
-int size(Lista *l); // retornar o tamanho da lista
-void print(Lista *l); // imprimir os elementos da lista
+void lista_append(Lista *l, int valor);
+void lista_insert(Lista *l, int valor);
+void lista_insert_sorted(Lista *l, int valor); // inserir ordenado
+void lista_clear(Lista *l);
+int lista_get(Lista *l, int index);
+int lista_full(Lista *l); // está cheia?
+int lista_empty(Lista *l); // está vazia?
+void lista_remove(Lista *l, int index); // remover por índice
+int lista_size(Lista *l); // retornar o tamanho da lista
+void lista_print(Lista *l); // imprimir os elementos da lista
